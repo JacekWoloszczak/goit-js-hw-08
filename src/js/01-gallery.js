b87b870ff4432a240f14b51c9b27624f2ce8f27a
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-10-09 21:06:58 +0200

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,44 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
-console.log(galleryItems);
+function galleryNew(image) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
 
-const body = document.querySelector('body');
-const div = document.createElement('div');
-div.classList.add('gallery__item');
-
-body.append(div);
-console.log(body);
-const galleryUl = document.querySelector('.gallery');
-div.append(galleryUl);
-
-galleryItems.forEach(i => {
-  const list = document.createElement('li');
-  galleryUl.append(list);
   const link = document.createElement('a');
-  link.href = i.original;
   link.classList.add('gallery__link');
+  link.href = image.original;
 
-  list.append(link);
   const img = document.createElement('img');
   img.classList.add('gallery__image');
-  img.src = i.preview;
-  img.alt = i.description;
-  img.setAttribute('data-source', i.original);
-  link.append(img);
+  img.src = image.preview;
+  img.alt = image.description;
 
-  img.addEventListener('click', event => {
-    event.preventDefault();
-    const selectedImage = event.target.dataset.source;
-    console.log(selectedImage);
-    const instance = basicLightbox.create(`
-    <div class="modal">
-    <img src="${selectedImage}"width="900" height="600">
-    </div>
-`);
+  link.appendChild(img);
+  galleryItem.appendChild(link);
 
-    instance.show();
-  });
-});
+  return galleryItem;
+}
+
+const gallery = document.querySelector('.gallery');
+const html = galleryItems.map(galleryNew);
+gallery.append(...html);
+
+const options = {
+  captions: true,
+  captionsDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+};
+const box = new SimpleLightbox('.gallery a', options);
